0ea24ce6a09000c046456781c82a1686d2e08c22
João Albergaria <jvalbergariabar@gmail.com>
2025-05-16 15:12:49 -0300

    Versão Robocore

--- Testes/teste_temperatura/teste_temperatura.ino
+++ Testes/teste_temperatura/teste_temperatura.ino
@@ -1,29 +1,29 @@
+// * Copyright 2019 RoboCore.
+// * Escrito por Renan Piva (10/04/2019).
+
+
+// Inclusao das bibliotecas
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int PINO_ONEWIRE = 12;
-OneWire oneWire(PINO_ONEWIRE);
-DallasTemperature sensor(&oneWire);
-DeviceAddress endereco_temp;
+const int PINO_ONEWIRE = 12; // Define pino do sensor
+OneWire oneWire(PINO_ONEWIRE); // Cria um objeto OneWire
+DallasTemperature sensor(&oneWire); // Informa a referencia da biblioteca dallas temperature para Biblioteca onewire
+DeviceAddress endereco_temp; // Cria um endereco temporario da leitura do sensor
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Medindo Temperatura");
-  sensor.begin();
-  
-  if (!sensor.getAddress(endereco_temp, 0)) {
-    Serial.println("SENSOR NAO CONECTADO");
-  }
+  Serial.begin(9600); // Inicia a porta serial
+  Serial.println("Medindo Temperatura"); // Imprime a mensagem inicial
+  sensor.begin(); ; // Inicia o sensor
 }
-
+  
 void loop() {
-  sensor.requestTemperatures();
-  if (sensor.isConnected(endereco_temp)) {
-    float temperatura = sensor.getTempC(endereco_temp);
-    Serial.print("Temperatura = ");
-    Serial.println(temperatura, 1);
+  sensor.requestTemperatures(); // Envia comando para realizar a conversão de temperatura
+  if (!sensor.getAddress(endereco_temp,0)) { // Encontra o endereco do sensor no barramento
+    Serial.println("SENSOR NAO CONECTADO"); // Sensor conectado, imprime mensagem de erro
   } else {
-    Serial.println("SENSOR DESCONECTADO DURANTE LEITURA");
+    Serial.print("Temperatura = "); // Imprime a temperatura no monitor serial
+    Serial.println(sensor.getTempC(endereco_temp), 1); // Busca temperatura para dispositivo
   }
   delay(1000);
 }
